a25e3804efab7339dce828de3c349eb795cf483a
fjodoin <143029509+fjodoin@users.noreply.github.com>
2025-03-04 19:10:15 -0500

    Update azure_resource_graph_collector.py

--- azure_resource_graph/azure_resource_graph_collector.py
+++ azure_resource_graph/azure_resource_graph_collector.py
@@ -21,7 +21,7 @@ RESOURCE_COLORS = {
     "Subscription": "#FFD700",
     "SystemAssignedManagedIdentity": "#98df8a",
     "UserAssignedManagedIdentity": "#FF7F0E",
-    "Principal": "#000000",
+    "Principal": "#313639",
     "FederatedCredential": "#9edae5"
 }
 
